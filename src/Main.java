ae0ecb479ffdaf4874b17b4dcea946fc6ab4f29e
glerique <gael.lerique@gmail.com>
2025-05-05 14:53:23 +0200

    feat(main): integrate greeting functionality in Main class (#3)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import greeting.model.User;
+import greeting.service.Greeter;
+import greeting.util.MessagePrinter;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        User user = new User("Gilbert");
+        Greeter greeter = new Greeter();
+        MessagePrinter printer = new MessagePrinter();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String message = greeter.sayHello(user);
+        printer.print(message);
     }
 }

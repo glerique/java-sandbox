686f826cd496eef4644f7adba8a8b76867bef48e
glerique <gael.lerique@gmail.com>
2025-05-06 21:23:10 +0200

    refactor(collections): implement TreeMapProcessor for TreeMap collections (#7)

--- src/collections/TreeMapProcessor.java
+++ src/collections/TreeMapProcessor.java
@@ -3,4 +3,7 @@ package collections;
 import java.util.TreeMap;
 
 public class TreeMapProcessor<K, V> extends AbstractCollectionProcessor<TreeMap<K, V>> {
+    public TreeMapProcessor() {
+        super("TreeMap");
+    }
 }

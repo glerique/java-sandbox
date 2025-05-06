67d1b01744fa68f2cc9214ff9ec703a03759656d
glerique <gael.lerique@gmail.com>
2025-05-06 21:21:46 +0200

    refactor(collections): implement LinkedListProcessor for LinkedList collections (#7)

--- src/collections/LinkedListProcessor.java
+++ src/collections/LinkedListProcessor.java
@@ -3,4 +3,7 @@ package collections;
 import java.util.LinkedList;
 
 public class LinkedListProcessor<T> extends AbstractCollectionProcessor<LinkedList<T>> {
+    public LinkedListProcessor() {
+        super("LinkedList");
+    }
 }

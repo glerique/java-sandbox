fbb5d2d3ab0fccf4f5c025659b9c8e8e10e302e3
glerique <gael.lerique@gmail.com>
2025-05-06 21:22:17 +0200

    refactor(collections): implement ArrayListProcessor for ArrayList collections (#7)

--- src/collections/ArrayListProcessor.java
+++ src/collections/ArrayListProcessor.java
@@ -3,4 +3,7 @@ package collections;
 import java.util.ArrayList;
 
 public class ArrayListProcessor<T> extends AbstractCollectionProcessor<ArrayList<T>> {
+    public ArrayListProcessor() {
+        super("ArrayList");
+    }
 }

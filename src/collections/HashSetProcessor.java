4b739f3cfe49782d45145ea6e2da98469eb15292
glerique <gael.lerique@gmail.com>
2025-05-06 21:20:50 +0200

    refactor(collections): implement HashSetProcessor for HashSet collections (#7)

--- src/collections/HashSetProcessor.java
+++ src/collections/HashSetProcessor.java
@@ -3,4 +3,7 @@ package collections;
 import java.util.HashSet;
 
 public class HashSetProcessor<T> extends AbstractCollectionProcessor<HashSet<T>> {
+    public HashSetProcessor() {
+        super("HashSet");
+    }
 }

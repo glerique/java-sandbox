e0de1291fd38caf41768c484688b1f50c75a4ef0
glerique <gael.lerique@gmail.com>
2025-05-06 21:20:18 +0200

    refactor(collections): implement ListProcessor for List collections (#7)

--- src/collections/ListProcessor.java
+++ src/collections/ListProcessor.java
@@ -3,4 +3,7 @@ package collections;
 import java.util.List;
 
 public class ListProcessor<T> extends AbstractCollectionProcessor<List<T>> {
+    public ListProcessor() {
+        super("List");
+    }
 }

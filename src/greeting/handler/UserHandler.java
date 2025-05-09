c06df2c367d393e0216562e1a353bb7797ec055e
glerique <gael.lerique@gmail.com>
2025-05-09 14:20:53 +0200

    refactor(handler): delegate user validation to UserValidator in UserHandler (#13)

--- src/greeting/handler/UserHandler.java
+++ src/greeting/handler/UserHandler.java
@@ -1,5 +1,6 @@
 package greeting.handler;
 
+import greeting.exception.IExceptionHandler;
 import greeting.model.User;
 import greeting.service.Greeter;
 import greeting.util.MessagePrinter;
@@ -7,14 +8,20 @@ import greeting.util.MessagePrinter;
 public class UserHandler {
     private final Greeter greeter;
     private final MessagePrinter printer;
+    private final IExceptionHandler exceptionHandler;
 
-    public UserHandler(Greeter greeter, MessagePrinter printer) {
+    public UserHandler(Greeter greeter, MessagePrinter printer, IExceptionHandler exceptionHandler) {
         this.greeter = greeter;
         this.printer = printer;
+        this.exceptionHandler = exceptionHandler;
     }
 
     public void greetUser(User user) {
-        String message = greeter.sayHello(user);
-        printer.print(message);
+        try {
+            String message = greeter.sayHello(user);
+            printer.print(message);
+        } catch (Exception e) {
+            exceptionHandler.handle(e);
+        }
     }
 }
